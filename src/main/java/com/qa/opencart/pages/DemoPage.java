131c86f47d2990ce944f03507d998344444964f6
Ganesh <gchenkalvarayan@ilinksolutions.com>
2025-07-26 12:49:36 -0400

    Added Demo Page

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+
+    By loc = By.id("Demo");
+    int i = 10;
+    public void get(){
+        System.out.println("Click on Demo");
+    }
+}
