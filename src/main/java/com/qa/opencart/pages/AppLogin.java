4d84bbaf2a7710264355dd5be01f97bcfe9eb3d5
Ganesh <gchenkalvarayan@ilinksolutions.com>
2025-07-27 09:04:46 -0400

    AppLogin Page

---
+++ src/main/java/com/qa/opencart/pages/AppLogin.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class AppLogin {
+
+    String name = "Ganesh";
+    public void login(){
+        System.out.println("Ganesh--Login");
+    }
+
+
+}
--- src/main/java/com/qa/opencart/pages/TestPage.java
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -2,6 +2,11 @@ package com.qa.opencart.pages;
 
 public class TestPage{
 
+  /***
+   *
+   * This is a a new feature
+   * @param args
+   */
   public static void main(String[] args){
 
     System.out.println("hello world");
