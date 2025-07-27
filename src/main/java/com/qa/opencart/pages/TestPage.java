22f928d520e73cb3a00b775e7f9d8ed1371c3f83
GaneshChen <gchenkalvarayan@ilinksolutions.com>
2025-07-27 11:14:20 -0400

    Update TestPage.java

--- src/main/java/com/qa/opencart/pages/TestPage.java
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -10,6 +10,9 @@ public class TestPage{
   public static void main(String[] args){
 
     System.out.println("hello world");
+    System.out.println("hello world1");
+    System.out.println("hello world2");
+    System.out.println("hello world3");
   }
 
 }
